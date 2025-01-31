ca37926483f6bc4a9280b36567666fde96f52e31
wlpar305 <2942932039@qq.com>
2025-01-31 11:48:52 +0800

    update bootstrap

--- ex-provider/src/main/java/com/rpc/provider/ProviderExample.java
+++ ex-provider/src/main/java/com/rpc/provider/ProviderExample.java
@@ -1,29 +1,18 @@
 package com.rpc.provider;
 
 import com.rpc.common.service.UserService;
-import com.rpc.myrpc.RpcApplication;
 import com.rpc.myrpc.bootstrap.ProviderBootstrap;
-import com.rpc.myrpc.config.RegistryConfig;
-import com.rpc.myrpc.config.RpcConfig;
-import com.rpc.myrpc.model.ServiceMetaInfo;
 import com.rpc.myrpc.model.ServiceRegisterInfo;
-import com.rpc.myrpc.registry.LocalRegistry;
-import com.rpc.myrpc.registry.Registry;
-import com.rpc.myrpc.registry.RegistryFactory;
-import com.rpc.myrpc.server.HttpServer;
-import com.rpc.myrpc.server.VertxHttpServer;
-import com.rpc.myrpc.server.tcp.VertxTcpServer;
 
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProviderExample {
     public static void main(String[] args) {
-        // 要注册的服务
         List<ServiceRegisterInfo<?>> serviceRegisterInfoList = new ArrayList<>();
         ServiceRegisterInfo serviceRegisterInfo = new ServiceRegisterInfo(UserService.class.getName(), UserServiceImpl.class);
         serviceRegisterInfoList.add(serviceRegisterInfo);
-
-        // 服务提供者初始化
         ProviderBootstrap.init(serviceRegisterInfoList);
     }
 }
